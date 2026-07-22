dc9d784b281609a05e364f63b0bfe5eb28527e20
Keerthi Sri Budharouthula <keerthibudharowthula@gmail.com>
2026-07-22 18:26:39 +0530

    Add files via upload

---
+++ Day-1/python oop memory allocation.py
@@ -0,0 +1,22 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name = "Ravi"
+>>> name
+'Ravi'
+>>> price = 500
+>>> price
+500
+>>> name = Laptop
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    name = Laptop
+NameError: name 'Laptop' is not defined
+>>> name = 'Laptop'
+>>> price=50000
+>>> rating=4.5
+>>> name
+'Laptop'
+>>> price
+50000
+>>> rating
+4.5
