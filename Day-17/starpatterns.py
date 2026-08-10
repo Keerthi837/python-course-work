01e908761546c31c973323cbcc199c9440d74b87
Keerthi Sri Budharouthula <keerthibudharowthula@gmail.com>
2026-08-10 19:12:52 +0530

    Add files via upload

---
+++ Day-17/starpatterns.py
@@ -0,0 +1,21 @@
+n = int(input("Enter the size:"))
+for i in range(n):
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+
+n =int(input("Enter the size:"))
+for i in range(n):
+    for sp in range(n-i-1):
+        print(' ',end=' ')
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+
+n =int(input("Enter the size:"))
+for i in range(n):
+    for sp in range(i):
+        print(' ',end=' ')
+    for j in range(n-i):
+        print('*',end=' ')
+    print()
